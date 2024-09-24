eab9a103a94d2f174a1507441dc1a0027c72eb21
Moahmmed Zaki <63197575+mohammedzaki7@users.noreply.github.com>
2024-09-24 20:15:08 +0300

    resolving review comments - changed the name of the database engine

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append("d:\irrigation-project\.venv\lib\site-packages")
 
 from fastapi import FastAPI
-from utils.database import Base, engine
+from utils.database import BaseDbEntity, db_engine
 from routes.authentication import router as authentication_router
 from routes.irrigation_scheduling import router as irrigation_scheduling_router
 from models.user import UserDB
@@ -11,7 +11,7 @@ from models.user import UserDB
 app = FastAPI()
 
 # Create the tables in the database
-Base.metadata.create_all(bind=engine)
+BaseDbEntity.metadata.create_all(bind=db_engine)
 
 app.include_router(authentication_router)
 app.include_router(irrigation_scheduling_router)
--- utils/database.py
+++ utils/database.py
@@ -6,13 +6,13 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "sqlite:///./irrigation.db"
 
 # Create a database engine
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+db_engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 # Create a base class for ORM models
 BaseDbEntity = declarative_base()
 
 # Create a session local class for database access
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
 
 # Dependency for database session
